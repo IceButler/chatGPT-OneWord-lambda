1f9dbfe692a443a239783bf81142d396e98a7c6d
kingchan223 <kokiyo97@naver.com>
2023-04-12 16:41:35 +0900

    lambda 코드 완

---
+++ index.js
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const OpenAI = require("openai");
+const { Configuration, OpenAIApi } = OpenAI;
+const bodyParser = require("body-parser");
+
+const configuration = new Configuration({
+  organization: process.env.ORGANIZATION,
+  apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(bodyParser.json());
+
+export const handler = async (event, context, callback) => {
+  const { keyword } = event;
+  const message = `'${keyword}'에서 대표 단어 한어절`;
+  let oneword = "";
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: message,
+    max_tokens: 4000,
+    temperature: 0,
+  });
+  if (response.data) {
+    if (response.data.choices)
+      oneword = getOneWord(response.data.choices[0].text);
+    else oneword = "none";
+  }
+};
+
+function getOneWord(text) {
+  if (text) {
+    let startIndex = text.indexOf('"');
+    let lastIndex = text.lastIndexOf('"');
+    return text.substring(startIndex + 1, lastIndex);
+  }
+}
